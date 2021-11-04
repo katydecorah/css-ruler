0a0038d81a0a45ef27b615866b814d16bb574915
Katy DeCorah <katydecorah@gmail.com>
2021-11-04 19:10:13 -0400

    :wrench: Update dependencies

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-css-ruler
-===========
+# css-ruler
 
 :straight_ruler: Explore CSS lengths.
 
@@ -7,10 +6,10 @@ css-ruler
 
 [According to MDN](https://developer.mozilla.org/en-US/docs/Web/CSS/length#Summary):
 
->  Please note that although `<percentage>` values are also CSS dimensions and are accepted by some CSS properties that accept `<length>` values, they are not themselves, `<length>` values.
+> Please note that although `<percentage>` values are also CSS dimensions and are accepted by some CSS properties that accept `<length>` values, they are not themselves, `<length>` values.
 
 ### Resources
 
-* https://developer.mozilla.org/en-US/docs/Web/CSS/length
-* https://dev.w3.org/csswg/css-values/#lengths
-* https://css-tricks.com/the-lengths-of-css/
+- https://developer.mozilla.org/en-US/docs/Web/CSS/length
+- https://dev.w3.org/csswg/css-values/#lengths
+- https://css-tricks.com/the-lengths-of-css/
--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-var myApp = angular.module('cssUnits', [], function ($interpolateProvider) {
-  $interpolateProvider.startSymbol('[[');
-  $interpolateProvider.endSymbol(']]');
+const myApp = angular.module("cssUnits", [], ($interpolateProvider) => {
+  $interpolateProvider.startSymbol("[[");
+  $interpolateProvider.endSymbol("]]");
 });
 
-myApp.controller('controls', ['$scope', function($scope) {
-  $scope.unit = 1;
-  $scope.bodySize = 16;
-  $scope.htmlSize = 16;
-  $scope.fontrelative = true;
-  $scope.viewportpercentage = true;
-  $scope.absolute = true;
-}]);
+myApp.controller("controls", [
+  "$scope",
+  function ($scope) {
+    $scope.unit = 1;
+    $scope.bodySize = 16;
+    $scope.htmlSize = 16;
+    $scope.fontrelative = true;
+    $scope.viewportpercentage = true;
+    $scope.absolute = true;
+  },
+]);
 
-$( document ).ready(function() {
+$(document).ready(() => {
   $("[data-toggle=popover]").popover({
-    placement: 'bottom',
-    trigger: 'hover'
+    placement: "bottom",
+    trigger: "hover",
   });
 });
